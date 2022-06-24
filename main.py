5fc53b0e83124db079413e52bbfdc030bce7d57f
Brian <brianbatz03@gmail.com>
2022-06-24 01:50:16 -0600

    Prueba de OpenCV

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 
 image = cv2.imread('src/src.png')
 alto, ancho, _ = image.shape
@@ -9,8 +10,19 @@ alto, ancho, _ = image.shape
 # imgOut = cv2.warpAffine(image, M, (ancho, alto+1500))
 
 # Rotación
-M = cv2.getRotationMatrix2D((ancho//2, alto//2), 180, -0.5)
-imgOut = cv2.warpAffine(image, M, (alto, ancho))
+# M = cv2.getRotationMatrix2D((ancho//2, alto//2), 90, 0.5)
+# imgOut = cv2.warpAffine(image, M, (alto, ancho))
+
+# Escalado
+# imgOut = cv2.resize(image,(361, 250), interpolation=cv2.INTER_CUBIC)
+
+# Escalado relación/aspecto
+# imgOut = imutils.resize(image, width=300)
+# imgOut = imutils.resize(image, height=300)
+
+# Recortar una imagen
+print('image.shape=', image.shape)
+imgOut = image[110:350, 100:300]
 
 cv2.imshow('Imagen de entrada', image)
 cv2.imshow('Imagen de salida', imgOut)
