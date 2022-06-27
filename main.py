d5d74c040ba805398c4673aba701fe3846338a8c
MartiPJ <61391952+MartiPJ@users.noreply.github.com>
2022-06-27 00:21:59 -0600

    nuevo main

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ alto, ancho, _ = image.shape
 M = cv2.getRotationMatrix2D((alto//2, ancho//2), 13, 0.5)
 imgOut = cv2.warpAffine(image, M, (alto, ancho))
 
+#escala de imagen
+def escala(x,y):
+    imageOut = cv2.resize(image,(x,y), interpolation=cv2.INTER_CUBIC)
+    plt.imshow(imageOut)
+
+#corta imagen
+def cut_image(x1,y1,x2,y2):
+    imageOut2= image[x1:y1,x2:y2]
+    plt.imshow(imageOut2)
+
 # Mostrar imágen
 plt.imshow(imgOut)
 plt.show()
